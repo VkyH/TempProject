e888d7191815b1a2c0af93052e8dddd5a1d1f563
VkyH <93672463+VkyH@users.noreply.github.com>
2023-02-03 18:43:37 +0530

    api done

---
+++ tempA/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Person(models.Model):
+    name=models.CharField(max_length=100,null=True,blank=True)
+    image=models.ImageField(upload_to='missing_persons')
+
+    def __str__(self):
+        return self.name
