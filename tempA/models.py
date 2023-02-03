2818432f09f9afd133fad05f71e90c78e7ab19bf
VkyH <93672463+VkyH@users.noreply.github.com>
2023-02-03 18:49:10 +0530

    images not required fields

--- tempA/models.py
+++ tempA/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Person(models.Model):
     name=models.CharField(max_length=100,null=True,blank=True)
-    image=models.ImageField(upload_to='missing_persons')
+    image=models.ImageField(upload_to='missing_persons',blank=True,null=True)
 
     def __str__(self):
         return self.name
